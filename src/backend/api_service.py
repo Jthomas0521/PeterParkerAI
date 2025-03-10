d0a84d4c4d065d13f26ec980da041e308ebd91ec
jahquan.thomas <jahquan.thomas@caci.com>
2025-03-10 16:42:22 -0400

    fixed dockercompose errors

--- src/backend/api_service.py
+++ src/backend/api_service.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 app = FastAPI()
 
+
 @app.get("/crawl")
 def crawl(url: str, selenium: bool = os.getenv("USE_SELENIUM", "False").lower() == "true"):
     result = crawl_website(url, use_selenium=selenium)
--- src/backend/crawler.py
+++ src/backend/crawler.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def crawl_website(url, use_selenium=False):
     if use_selenium:
         options = Options()
@@ -19,21 +20,25 @@ def crawl_website(url, use_selenium=False):
         html_driver = driver.page_source
         html_driver.quit()
     else:
-        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
         response.raise_for_status()
         html_driver = response.text
 
-    # Parse HTML content    
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Parse HTML content
+    soup = BeautifulSoup(response.text, "html.parser")
 
     # Extract data
     title = soup.title.string if soup.title else "No tile found."
-    headers = [header.get_text(strip = True) for header in soup.find_all(['h1', 'h2', 'h3'])]
-    paragraphs = [p.get_text(strip = True) for p in soup.find_all('p')]
-    links = [a['href'] for a in soup.find_all('a', href=True)]
+    headers = [header.get_text(strip=True) for header in soup.find_all(["h1", "h2", "h3"])]
+    paragraphs = [p.get_text(strip=True) for p in soup.find_all("p")]
+    links = [a["href"] for a in soup.find_all("a", href=True)]
 
     # Convert to DataFrame
-    data = {'title': [title], 'headers': [headers], 'paragraphs': [paragraphs[:5]], 'links': [links[:5]],
+    data = {
+        "title": [title],
+        "headers": [headers],
+        "paragraphs": [paragraphs[:5]],
+        "links": [links[:5]],
     }
 
     return pd.DataFrame(data)
